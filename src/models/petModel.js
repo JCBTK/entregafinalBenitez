2dd38939253b1f562a8f7dca5360f0daa8bace04
JCBTK <59676003+JCBTK@users.noreply.github.com>
2024-12-24 17:43:38 -0300

    Revert "cambios"
    This reverts commit 80cb3fcd987cb37b7326e88275b26ac786c85960.

--- src/models/petModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const petSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    type: { type: String, required: true },
-    age: { type: Number, required: true, min: 0 },
-});
-const Pet = mongoose.model('Pet', petSchema);
-export default Pet;
